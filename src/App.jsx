0d4afd9c1fdd4b3aced9699920df265748eadc29
Cody Garcia <czgarcia@berkeley.edu>
2024-08-22 01:33:37 -0700

    abstracted app a bit, added scene component

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Project3_Card from './Project3_Card';
 import Project4_Card from './Project4_Card';
 import Project5_Card from './Project5_Card';
 import Project6_Card from './Project6_Card';
+import Scene from './Scene';
 
 function App() {
 
@@ -35,24 +36,7 @@ function App() {
       >
         Click here to go back
       </div>
-      <Canvas camera={{ position: [15, 10, 10], fov: 50 }}>
-        <OrbitControls />
-        <ambientLight intensity={0.5} />
-        <directionalLight intensity={2} position={[-5, 5, 5]} castShadow shadow-mapSize={2048} shadow-bias={-0.0001} />
-        <ScrollControls damping={0.2} maxSpeed={0.5} pages={2}>
-          <group>
-            <Wallet onWalletClick={handleWalletClick} isOpened={isOpened} />
-            <DL_Card />
-            <Business_Card />
-            <Project1_Card isOpened={isOpened} />
-            <Project2_Card isOpened={isOpened} />
-            <Project3_Card isOpened={isOpened} />
-            <Project4_Card isOpened={isOpened} />
-            <Project5_Card isOpened={isOpened} />
-            <Project6_Card isOpened={isOpened} />
-          </group>
-        </ScrollControls>            
-      </Canvas>
+      <Scene handleWalletClick={handleWalletClick} isOpened={isOpened} />
     </>
   )
 }
---
+++ src/Scene.jsx
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+import { useRef, useState } from 'react'
+import { Suspense } from 'react'
+import { extend } from '@react-three/fiber'
+import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useGLTF, useAnimations, useScroll, ScrollControls, Float, Html } from "@react-three/drei"
+
+import './App.css'
+
+import Wallet from './wallet';
+import DL_Card from './dl_card';
+import Business_Card from './Business_Card';
+import Project1_Card from './Project1_Card';
+import Project2_Card from './Project2_Card';
+import Project3_Card from './Project3_Card';
+import Project4_Card from './Project4_Card';
+import Project5_Card from './Project5_Card';
+import Project6_Card from './Project6_Card';
+
+export default function Scene( {handleWalletClick, isOpened} ) {
+
+  return (
+    <>
+      <Canvas camera={{ position: [15, 10, 10], fov: 50 }}>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <directionalLight intensity={2} position={[-5, 5, 5]} castShadow shadow-mapSize={2048} shadow-bias={-0.0001} />
+        <ScrollControls damping={0.2} maxSpeed={0.5} pages={2}>
+          <group>
+            <Wallet onWalletClick={handleWalletClick} isOpened={isOpened} />
+            <DL_Card />
+            <Business_Card />
+            <Project1_Card isOpened={isOpened} />
+            <Project2_Card isOpened={isOpened} />
+            <Project3_Card isOpened={isOpened} />
+            <Project4_Card isOpened={isOpened} />
+            <Project5_Card isOpened={isOpened} />
+            <Project6_Card isOpened={isOpened} />
+          </group>
+        </ScrollControls>            
+      </Canvas>
+    </>
+  )
+}
